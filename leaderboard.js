bdc8a13de397c0840eaf25ca202932eea2f08103
meteortips <hello@meteortips.com>
2014-10-29 12:28:40 +1100

    Your First Meteor Application - Templates

--- leaderboard.js
+++ leaderboard.js
@@ -1,3 +1,23 @@
 
 // Create a MongoDB Collection
 PlayersList = new Mongo.Collection('players');
+
+// Code that only runs on the client (within the web browser)
+if(Meteor.isClient){
+
+  // Helper functions execute code within templates
+  Template.leaderboard.helpers({
+    'player': function(){
+
+        // Retrieve all of the data from the "PlayersList" collection
+        return PlayersList.find()
+        
+    }
+  });
+
+}
+
+// Code that only runs on the server (where the application is hosted)
+if(Meteor.isServer){
+
+}
